0e4473ca8f7f7b0bede10714dbfb1d2f832c7ed1
LeeChangYoon <41320453+LeeChangYoon@users.noreply.github.com>
2021-08-05 13:47:37 +0900

    Add files via upload

---
+++ HBE_Robocar_Framework/PSD.h
@@ -0,0 +1,24 @@
+/*
+ * PSD.h
+ *
+ * Created: 04/08/2021
+ * Author: Lee Chang Yoon
+ */ 
+
+
+#ifndef PSD_H_
+#define PSD_H_
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "avr_lib.h"
+
+//	1)
+u16 PSD_val[15]={ 954, 745, 512, 397, 331, 283, 255, 224, 200, 188, 171, 164, 151, 116, 110};
+u08 PSD_cm[]=   { 5  , 10 , 15 , 20 , 25 , 30 , 35 , 45 , 50 , 55 , 60 , 65 , 70 , 75 , 80 };
+	
+int GetDistancePSD();
+
+
+
+#endif /* PSD_H_ */
---
+++ HBE_Robocar_Framework/Ports.h
@@ -0,0 +1,16 @@
+/*
+ * Ports.h
+ *
+ * Created: 04/08/2021
+ * Author: Lee Chang Yoon
+ */ 
+
+
+#ifndef PORTS_H_
+#define PORTS_H_
+
+void Initialize_Ports();
+
+
+
+#endif /* PORTS_H_ */
