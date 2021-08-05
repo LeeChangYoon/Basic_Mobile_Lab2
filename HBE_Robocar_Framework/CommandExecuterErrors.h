faee7c6e923c18ab3469b7c3ff81d6e79730f366
LeeChangYoon <41320453+LeeChangYoon@users.noreply.github.com>
2021-08-05 13:46:17 +0900

    Add files via upload

---
+++ HBE_Robocar_Framework/CommandExecuterErrors.h
@@ -0,0 +1,18 @@
+/*
+ * CommandExecuterErrors.h
+ *
+ * Created: 04/08/2021
+ * Author: Lee Chang Yoon
+ */ 
+
+
+#ifndef COMMANDEXECUTERERRORS_H_
+#define COMMANDEXECUTERERRORS_H_
+
+
+
+#define NO_SUCH_COMMAND_ERROR        -1
+#define COMMAND_EXECUTE_SUCCESS       1
+#define COMMAND_EXECUTE_FAIL          0
+
+#endif /* COMMANDEXECUTERERRORS_H_ */
